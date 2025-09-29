df2e9dc1be241482735c4dcf45e8055d64ea0690
iamandaleao <amandabibiano144@gmail.com>
2025-09-29 20:35:05 -0300

    chore: commit inicial

--- saida-java/src/PrintNumbers/Numero.java
+++ saida-java/src/PrintNumbers/Numero.java
@@ -9,3 +9,11 @@ public class Numero {
 }
 
 // Podemos usar o println() para imprimir os números entretanto, diferentemente do texto, não colocamos números entre aspas duplas
+
+
+// Podemos também realizar cálculos matemáticos dentro do println():
+
+/*
+System.out.println(3 +3);
+System.out.println(2*5);
+ */
