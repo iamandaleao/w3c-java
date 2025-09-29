93e78dd2502437649827fa2c172a5bc855c0ce12
iamandaleao <amandabibiano144@gmail.com>
2025-09-29 14:49:28 -0300

    feat: adiciona classe Concatenar

---
+++ inicio-base/src/Concatenar.java
@@ -0,0 +1,16 @@
+// 6. Concatenar (juntar textos e variáveis)
+
+class Concatenar {
+    public static void main(String[] args) {
+        String nome = "Leão";
+        int idade = 20;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+    }
+}
+
+// O sinal + serve para juntar texto com valores.
+// Clique em shift + f10 para gerar o código na tela ou no play na parte superior acima no programa intellij idea.
+// ctrl + f5 para gerar novamente
+// alt + f12 para abrir o terminal
