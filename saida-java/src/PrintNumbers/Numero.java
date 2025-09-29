9dfabc984c5ab6964d117c2343547011e1dea618
iamandaleao <amandabibiano144@gmail.com>
2025-09-29 15:53:46 -0300

    chore: commit inicial

---
+++ saida-java/src/PrintNumbers/Numero.java
@@ -0,0 +1,11 @@
+package PrintNumbers;
+
+public class Numero {
+    public static void main(String[] args) {
+        System.out.println(3);
+        System.out.println(358);
+        System.out.println(50000);
+    }
+}
+
+// Podemos usar o println() para imprimir os números entretanto, diferentemente do texto, não colocamos números entre aspas duplas
